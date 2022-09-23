0962c3f084c005264727efcce2b3362c30140d39
Samu K <samu.kaarlela@gmail.com>
2022-09-23 14:17:26 +0300

    Fixed sending of data to server. Date modified to be correct format. Allowed selection of income or expense

--- budget/main.cpp
+++ budget/main.cpp
@@ -4,8 +4,11 @@
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QString>
+#include <QSqlError>
 #include <chrono>
 #include <ctime>
+#include <QCoreApplication>
+#include <time.h>
 
 using namespace std;
 
@@ -35,19 +38,36 @@ void setup_db(QString password) {
 
     //
     db.setDatabaseName(db_url);
+    db.setUserName("budgetaccess");
+    db.setPassword("So6X7dnFdN");
+
     db.open();
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
+    QCoreApplication a(argc,argv);
+
     // Setup variables
+    int type_mod;
+    cout << "Give transaction type (1 = expense, 2=income) ";
+    cin >> type_mod;
+    cin.clear();
+
     int amount;
     cout << "Input given amount: ";
     cin >> amount;
     cin.clear();
 
     time_t date = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    tm * date_tm;
+    char date_str[100];
+
+    time(&date);
+    date_tm = localtime(&date);
+
+    strftime(date_str,50,"%Y-%m-%d",date_tm);
 
     string vendor;
     cout << "Input vendor: ";
@@ -60,19 +80,48 @@ int main()
     cin.clear();
 
     // Filepath to password
-    string fp = "../keys/azure_pass.txt";
+    string fp = "../../keys/azure_pass.txt";
     QString password = fetch_password(fp);
 
     // Setup our database
-    setup_db(password);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+
+    // Construct the url
+    QString db_url = QString("Driver={ODBC Driver 13 for SQL Server};Server=tcp:budgetdata.database.windows.net,1433;Database=budget;Uid=budgetaccess;Pwd={")
+            + password
+            + QString("};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;");
+
+    //
+    db.setDatabaseName(db_url);
+    //db.setUserName("budgetaccess");
+    //db.setPassword(password);
+    db.open();
+
+    QString er = db.lastError().text();
+    string error_line = er.toStdString();
+
+    cout << "Error: \n" << error_line << endl;
 
     QSqlQuery query;
-    query.prepare("INSERT INTO expenses "
-                    "VALUES (:date,:amount,:vendor,:category)");
-    query.bindValue(":date",QString::fromStdString(ctime(&date)));
+    if (type_mod == 1) {
+        query.prepare("INSERT INTO expenses "
+                        "VALUES (:date,:amount,:vendor,:category)");
+
+    } else if (type_mod == 2) {
+        query.prepare("INSERT INTO income "
+                        "VALUES (:date,:amount,:vendor,:category)");
+    }
+
+    query.bindValue(":date",QString::fromStdString(date_str));
     query.bindValue(":amount",amount);
     query.bindValue(":vendor",QString::fromStdString(vendor));
     query.bindValue(":category",category);
+
     query.exec();
+
+    QString qer = query.lastError().text();
+    string qerror_line = qer.toStdString();
+
+    cout << "Error: \n" << qerror_line << endl;
 
 }
