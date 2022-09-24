381f5a92a5bf5961480931db9d1db78ddc46310b
Samu K <samu.kaarlela@gmail.com>
2022-09-25 00:52:13 +0300

    Refactored and cleaned python files

--- scripts/exeldata.py
+++ scripts/exeldata.py
@@ -1,21 +1,21 @@
+# Imports
 import numpy
 import pandas
 from datetime import datetime
 
+# Read our data to a dataframe
 fp = "income.csv"
-
 df = pandas.read_csv(fp)
 
-
+# Drop rows and columns that are only nan
 df.dropna(axis=1,how="all",inplace=True)
 df.dropna(axis=0,how="all",inplace=True)
 
+# Convert data to correct format and type
 df["date"] = df["date"].apply(lambda x: datetime.strptime(x,"%d.%m.%Y"))
 df["date"] = df["date"].apply(lambda x: x.strftime("%Y-%m-%d"))
 df["category"] = df["category"].apply(lambda x: x.lower())
 df["amount"] = df["amount"].apply(lambda x: x.replace("$","").replace("€","").strip())
 
-print(df.head(5))
-
-
+# Save the cleaned csv to new file
 df.to_csv("clean_income.csv")
