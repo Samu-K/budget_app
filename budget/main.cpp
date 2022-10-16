d731c76c5b290ac1b5599e36c3569cf48815fbb3
Samu-K <samu.kaarlela@gmail.com>
2022-10-16 19:16:15 +0300

    Cleaned main.cpp

--- budget/main.cpp
+++ budget/main.cpp
@@ -1,28 +1,16 @@
-// Include libraries for SQL
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QString>
-#include <QSqlError>
-
-// Libraries for time and date
-#include <ctime>
-#include <chrono>
-
-// Other useful libraries
+// Include needed libraries
 #include <QApplication>
-#include <iostream>
-#include <string>
-#include <fstream>
-
 #include "budget.h"
 
 using namespace std;
 
+// start the program up
 int main(int argc, char *argv[])
 {
     // Init core applicattion
-    // Will be expanded and changed when UI is created
     QApplication app(argc,argv);
+
+    // init budget class
     Budget window;
     window.show();
 
