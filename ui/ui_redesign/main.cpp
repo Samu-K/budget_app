d9e94ad771107a9c4ed22f4218b64b9a9819af03
Samu-K <samu.kaarlela@gmail.com>
2023-02-01 12:36:27 +0200

    Changed main to use actual Program again, instead of placeholder main.qml used for dev

--- ui/ui_redesign/main.cpp
+++ ui/ui_redesign/main.cpp
@@ -9,10 +9,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QQmlComponent component(&engine, url);
-    component.create();
-//    Program prog(engine);
+    Program prog(engine);
 
     return app.exec();
 }
